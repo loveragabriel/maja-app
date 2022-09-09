dcb42d5ed479ce7e1c6712892e3f75457e1905be
loveragabriel <loveragabriel20@gmail.com>
2022-09-08 22:10:45 -0300

    img responsive fluid

--- src/Component/PrincipalImg.js
+++ src/Component/PrincipalImg.js
@@ -4,8 +4,7 @@ export const PrincipalImg = () => {
   return (
     <div>
         <div className='Home'>
-
-            <img className='home-img' src='https://cdn.pixabay.com/photo/2022/01/08/08/07/dentist-6923507_960_720.jpg'></img>
+            <img className='img-fluid' src='https://cdn.pixabay.com/photo/2022/01/08/08/07/dentist-6923507_960_720.jpg'></img>
              <div className='description-text'>
             <p>
             La Odontología General (Operatoria o Restauradora) es la encargada de solucionar los problemas primarios que tienen que ver con la boca, dientes y otras estructuras relacionadas. Representa en la mayoría de los casos el contacto inicial del paciente con el profesional de la Odontología.
--- src/Component/Profile.js
+++ src/Component/Profile.js
@@ -3,8 +3,8 @@ import React from 'react'
 export const Profile = () => {
   return (
     <div>
-        <div className='profile-container'>
-            <img src='https://cdn.pixabay.com/photo/2018/01/04/14/16/woman-3060784_960_720.jpg'></img>
+        <div >
+            <img className='img-fluid' src='https://cdn.pixabay.com/photo/2018/01/04/14/16/woman-3060784_960_720.jpg'></img>
             <div>
             <p className='text-home-div'>Dra. Maria Alejandra</p>
             </div>        
