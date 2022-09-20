decd8229777550ca16756f77a380037e1b073734
loveragabriel <loveragabriel20@gmail.com>
2022-09-20 00:26:45 -0300

    footer page updated

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
       <footer className='mt-3 text-center '>
-      <small className="text-muted copy-right">Copy Right Gabriel Lovera 2022</small>
+      <small className="text-muted copy-right">Copyright © 2022 <a href={'https://www.linkedin.com/in/loveragabriel/'} target="_blank">Gabriel Lovera.</a></small>
       </footer>
   )
 }
