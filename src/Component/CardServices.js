76e45f3a6ee29c4c0027ad53404513ce6937c1b9
loveragabriel <loveragabriel20@gmail.com>
2022-09-20 00:00:00 -0300

    about page fixed

--- src/Component/CardServices.js
+++ src/Component/CardServices.js
@@ -5,18 +5,18 @@ import { Link } from 'react-router-dom'
 export const CardServices = () => {
   return (
     <div className='container mt-4 '>
-        <div className=' text-center'>
+      <div className=' text-center'>
         <hr></hr>
-            <div>
-                <h2 className='text-center mb-4 fs-2'>Tratamientos</h2>
-            </div>
-            <ul className='list-group list-unstyled tex-list'>
-                <li ><Link className='text-decoration-none text-warning'  to='Services'>Limpieza</Link></li>
-                <li ><Link className='text-decoration-none text-warning' to='Services'>Prótesis</Link></li>
-                <li ><Link className='text-decoration-none text-warning' to='Services'>Arreglos</Link></li>
-                <li ><Link className='text-decoration-none text-warning' to='Services'>Placas Miorrelajantes</Link></li>
-            </ul>
+        <div>
+          <h2 className='text-center mb-4 fs-2'>Tratamientos</h2>
         </div>
+        <ul className='list-group list-unstyled tex-list'>
+          <li ><Link className='text-decoration-none text-warning' to='Services'>Limpieza</Link></li>
+          <li ><Link className='text-decoration-none text-warning' to='Services'>Prótesis</Link></li>
+          <li ><Link className='text-decoration-none text-warning' to='Services'>Arreglos</Link></li>
+          <li ><Link className='text-decoration-none text-warning' to='Services'>Placas Miorrelajantes</Link></li>
+        </ul>
+      </div>
     </div>
   )
 }
